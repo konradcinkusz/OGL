6ed148d830c848e8e1ec8c7a8ec9412e5a5920bb
Konrad Cinkusz <konradcinkusz@gmail.com>
2017-12-05 22:57:18 +0100

    first commit

---
+++ Repozytorium/Migrations/Configuration.cs
@@ -0,0 +1,119 @@
+﻿namespace Repozytorium.Migrations
+{
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Models;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Repozytorium.Models.OglContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
+        }
+
+        protected override void Seed(Repozytorium.Models.OglContext context)
+        {
+            //if (!Debugger.IsAttached)
+            //    Debugger.Launch();
+            SeedRoles(context);
+            SeedUsers(context);
+            SeedOgloszenia(context);
+            SeedKategorie(context);
+            SeedOgloszenie_Kategoria(context);
+        }
+        private void SeedRoles(OglContext context)
+        {
+            RoleManager<IdentityRole> roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>());
+            if (!roleManager.RoleExists("Admin"))
+            {
+                IdentityRole role = new IdentityRole { Name = "Admin" };
+                roleManager.Create(role);
+            }
+            if (!roleManager.RoleExists("Pracownik"))
+            {
+                IdentityRole role = new IdentityRole { Name = "Pracownik" };
+                roleManager.Create(role);
+            }
+        }
+        private void SeedUsers(OglContext context)
+        {
+            UserStore<Uzytkownik> store = new UserStore<Uzytkownik>(context);
+            UserManager<Uzytkownik> manager = new UserManager<Uzytkownik>(store);
+            if (!context.Users.Any(u => u.UserName == "Admin"))
+            {
+                Uzytkownik user = new Uzytkownik { UserName = "Admin", Wiek = 12 };
+                IdentityResult adminResult = manager.Create(user, "123456789");
+                if (adminResult.Succeeded)
+                    manager.AddToRole(user.Id, "Admin");
+            }
+            if (!context.Users.Any(u => u.UserName == "Marek"))
+            {
+                Uzytkownik user = new Uzytkownik { UserName = "marek@AspNetMvc.pl", Wiek = 48 };
+                IdentityResult adminResult = manager.Create(user, "123456789");
+                if (adminResult.Succeeded)
+                    manager.AddToRole(user.Id, "Pracownik");
+            }
+            if (!context.Users.Any(u => u.UserName == "Prezes"))
+            {
+                Uzytkownik user = new Uzytkownik { UserName = "prezes@AspNetMvc.pl", Wiek = 33 };
+                IdentityResult adminResult = manager.Create(user, "123456789");
+                if (adminResult.Succeeded)
+                    manager.AddToRole(user.Id, "Admin");
+            }
+        }
+        private void SeedOgloszenia(OglContext context)
+        {
+            string idUzytkownika = context.Set<Uzytkownik>().Where(u => u.UserName == "Admin").FirstOrDefault().Id;
+            for (int i = 1; i <= 10; i++)
+            {
+                Ogloszenie ogl = new Ogloszenie()
+                {
+                    ID = i,
+                    UzytkownikId = idUzytkownika,
+                    Tresc = $"Treść ogłoszenia: {i}",
+                    Tytul = $"Tytul ogłoszenia: {i}",
+                    DataDodania = DateTime.Now.AddDays(-i)
+                };
+                context.Set<Ogloszenie>().AddOrUpdate(ogl);
+            }
+            context.SaveChanges();
+        }
+        private void SeedKategorie(OglContext context)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Kategoria kat = new Kategoria()
+                {
+                    Id = i,
+                    Nazwa = $"Nazwa kategorii {i}",
+                    Tresc = $"Tresc kategorii {i}",
+                    MetaTytul = $"Metatutl kategorii {i}",
+                    MetaOpis = $"metaopis kategorii {i}",
+                    MetaSlowa = $"Słowa kluczowe do kategorii {i}",
+                    ParentId = i
+                };
+                context.Set<Kategoria>().AddOrUpdate(kat);
+            }
+            context.SaveChanges();
+        }
+        private void SeedOgloszenie_Kategoria(OglContext context)
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                Ogloszenie_Kategoria okat = new Ogloszenie_Kategoria()
+                {
+                    Id = i,
+                    OgloszenieId = i /2 + 1,
+                    KategoriaId = i / 2 + 2
+                };
+                context.Set<Ogloszenie_Kategoria>().AddOrUpdate(okat);
+            }
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Repozytorium/Models/OglContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace Repozytorium.Models
+{
+    public class OglContext : IdentityDbContext
+    {
+        public OglContext() : base("DefaultConnection")
+        { 
+
+        }
+        public static OglContext Create()
+        {
+            return new OglContext();
+        }
+        public DbSet<Kategoria> Kategorie { get; set; }
+        public DbSet<Ogloszenie> Ogloszenie  { get; set; }
+        public DbSet<Uzytkownik> Uzytkownik  { get; set; }
+        public DbSet<Ogloszenie_Kategoria> Ogloszenie_Kategoria  { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Entity<Ogloszenie>().HasRequired(x => x.Uzytkownik).WithMany(x => x.Ogloszenia).HasForeignKey(x => x.UzytkownikId).WillCascadeOnDelete(true);
+        }
+    }
+}
